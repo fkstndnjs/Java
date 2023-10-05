a03c57a5fa9269535e6796c087a0ed2316f9e379
fkstndnjs <fkstndnjs@naver.com>
2023-10-06 00:15:07 +0900

    chore: class Runner

---
+++ Section3/Mul.java
@@ -0,0 +1,9 @@
+package Section3;
+
+public class Mul {
+    void print(){
+        for(int i = 1; i<=10; i++){
+            System.out.printf("%d * %d = %d", 5, i, 5*i).println();
+        }
+    }
+}
---
+++ Section3/MulRunner.java
@@ -0,0 +1,9 @@
+package Section3;
+
+public class MulRunner {
+    public static void main(String[] args) {
+        Mul temp  = new Mul();
+
+        temp.print();
+    }
+}
